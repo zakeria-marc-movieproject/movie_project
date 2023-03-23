291799d7372c0ac88e057e5b9d07df6e0827a2bd
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-03-23 13:02:45 -0500

    'editing more  things'

--- js/movies.js
+++ js/movies.js
@@ -1,79 +1,83 @@
 "use strict"
-let toggle = $('#load').toggleClass('hidden')
+let toggle = $('#reload').toggleClass('hidden')
 let addRating = document.getElementById("added-movie")
 let addTitle = document.getElementById('submit-title');
 let submitButton = document.querySelector('#submit-movie');
-let i = document.querySelector("#movieList");
-
-// document.getElementById("loading-gif").style.display = 'none';
 getList();
-$('#load').toggleClass('hidden')
+
+
 function getList() {
-    let movieList =document.getElementById('movieList')
+    $(document).ready(function () {
+        $('#reload').show();
+    })
     fetch(`https://just-cold-diver.glitch.me/movies`)
         .then(response => response.json())
-        .then(data => {
+        .then(movieList => {
             let html = "";
 
-            for (i = 0; i < data.length; i++) {
+            for (let i = 0; i < movieList.length; i++) {
                 html += `<div>`
-                html += `<h2>${data[i].title}</h2>`
-                html += `<h5>${data[i].rating}</h5>`
-                html += `<button name="SaveChanges" class="saveThis hidden container-fluid" type="submit" value="${data[i].id}">Save Changes</button>`
-                html += `<button name="Edit" class="editThis container-fluid" type="submit" value="${data[i].id}">Edit Details</button>`
-                html += `<button name="Delete" class="deletethis container-fluid" type="submit" value="${data[i].id}">Delete Movie</button>`
+                html += `<h1>${movieList[i].title}</h1>`
+                html += `<h4>${movieList[i].rating}</h4>`
+                html += `<h5>${movieList[i].genre}</h5>`
+                html += `<button name="SaveChanges" class="saveThis" type="submit" value="${movieList[i].id}">Save Changes</button>`
+                html += `<button name="Edit" class="editThis" type="submit" value="${movieList[i].id}">Edit Details</button>`
+                html += `<button name="Delete" class="deleteThis" type="submit" value="${movieList[i].id}">Delete Movie</button>`
                 html += `</div>`
-                console.log(data);
+                console.log(movieList);
             }
+            $('#movieList').html(html);
+            //      Hides loading image after movies displayed
+            $('#reload').toggle('hidden')
 
-            // delete button
-            $('.deletethis').click(function () {
-                deleteMovie($(this).val());
-                $('.editThis').click(function () {
-                    let editId = ($(this).val());
-                    let title = $(this).parent().children('h2').first().html()
-                    let rating = $(this).parent().children('h5').first().html()
-                    $(this).parent().children('.saveThis').toggleClass('hidden')
-                    $(this).toggleClass('hidden')
-                    $(this).parent().children('h2').first().html(`<input type='text' value='${title}' class="editarea container-fluid">`);
-                    $(this).parent().children('h5').first().html(editRating(rating));
+            // // delete button
+            // $('.deleteThis').click(function () {
+            //     deleteMovie($(this).val());
+            //     // edit button
+            //     $('.editThis').click(function () {
+            //         let editId = ($(this).val());
+            //         let title = $(this).parent().children('h2').first().html()
+            //         let rating = $(this).parent().children('h5').first().html()
+            //         $(this).parent().children('.saveThis').toggleClass('hidden')
+            //         $(this).toggleClass('hidden')
+            //         $(this).parent().children('h2').first().html(`<input type='text' value='${title}' class="editarea container-fluid">`);
+            //         $(this).parent().children('h5').first().html(editRating(rating));
+            // //
+            // //         // // Save Changes Button
+            //         $(".saveThis").click(function () {
+            //             let titleArea = $('.editarea').val();
+            //             let selectedRating = $("#selector").val();
+            //             editMovie(editId, titleArea, selectedRating);
+            //         });
+            //         //
+            //         // // Edit Movie (Title) save changes on enter
+            //         // $(".editarea").keyup(function (event) {
+            //         //     let keyStroke = event.key;
+            //         //     if (keyStroke === 'Enter') {
+            //         //         let textarea = $(this).val();
+            //         //         let selectedRating = $("#selector").val();
+            //         //         console.log(selectedRating);
+            //         //         editMovie(editId, textarea, selectedRating);
+            //         //     }
+            //         //
+            //         // });
+            //         // // Edit Movie (Rating) save changes on enter
+            //         // $(".ratingArea").keyup(function (event) {
+            //         //     let keyStroke = event.key;
+            //         //     if (keyStroke === 'Enter') {
+            //         //         let titleArea = $('.editarea').val();
+            //         //         let selectedRating = $("#selector").val();
+            //         //         editMovie(editId, titleArea, selectedRating);
+            //         //     }
+            //         // });
+            //     })
+            // })
 
-                    // Save Changes Button
-                    $(".saveThis").click(function () {
-                        let titleArea = $('.editarea').val();
-                        let selectedRating = $("#selector").val();
-                        editMovie(editId, titleArea, selectedRating);
-                    });
-
-                    // Edit Movie (Title) save changes on enter
-                    $(".editarea").keyup(function (event) {
-                        let keyStroke = event.key;
-                        if (keyStroke === 'Enter') {
-                            let textarea = $(this).val();
-                            let selectedRating = $("#selector").val();
-                            console.log(selectedRating);
-                            editMovie(editId, textarea, selectedRating);
-                        }
-
-                    });
-                    // Edit Movie (Rating) save changes on enter
-                    $(".ratingArea").keyup(function (event) {
-                        let keyStroke = event.key;
-                        if (keyStroke === 'Enter') {
-                            let titleArea = $('.editarea').val();
-                            let selectedRating = $("#selector").val();
-                            editMovie(editId, titleArea, selectedRating);
-                        }
-                    });
-                })
-            })
-                // Hides loading image after movies displayed
-                $('#load').toggleClass('hidden')
-            function addMovie(m) {
-                m.preventDefault();
+            function addMovie(name,rating) {
+                // m.preventDefault();
                 let movieObj = {
-                    title: addTitle.value,
-                    rating: addRating.value
+                    title: name,
+                    rating: rating
                 };
 
                 // Fetch post to movies json
@@ -83,87 +87,98 @@ function getList() {
                         'Content-Type': 'application/json',
                     },
                     body: JSON.stringify(movieObj),
-                }).then(() => fetch(`https://just-cold-diver.glitch.me/movies`)).then(resp => resp.json()).then(() => getList());
-                addTitle.value = ''; //resets typed value
-
+                }).then(() => fetch(`https://just-cold-diver.glitch.me/movies`))
+                    .then(resp => resp.json()).then(() => getList());
+                let html = "";
+                for (let i = 0; i < movieList.length; i++) {
+                    html += `<div>`
+                    html += `<h1>${movieList[i].title}</h1>`
+                    html += `<h4>${movieList[i].rating}</h4>`
+                    html += `<h5>${movieList[i].genre}</h5>`
+                    html += `<button name="SaveChanges" class="saveThis" type="submit" value="${movieList[i].id}">Save Changes</button>`
+                    html += `<button name="Edit" class="editThis" type="submit" value="${movieList[i].id}">Edit Details</button>`
+                    html += `<button name="Delete" class="deleteThis" type="submit" value="${movieList[i].id}">Delete Movie</button>`
+                    html += `</div>`
+                    console.log(movieList);
+                }
+                $('#movieList').html(html);
             }
 
 
 // Delete Movie list
-            function deleteMovie(movieId) {
-                fetch(`https://just-cold-diver.glitch.me/movies` + movieId, {
-                    method: "DELETE"
-                }).then(() => fetch(`https://just-cold-diver.glitch.me/movies`)).then(resp => resp.json()).then(() => getList());
-            }
+//             function deleteMovie(movieId) {
+//                 fetch(`https://just-cold-diver.glitch.me/movies` + movieId, {
+//                     method: "DELETE"
+//                 }).then(() => fetch(`https://just-cold-diver.glitch.me/movies`)).then(resp => resp.json()).then(() => getList());
+//             }
 
 // Edit Movie List
-            function editMovie(movieID, title, rating) {
-                let editedMovie = {
-                    title: title,
-                    rating: rating,
-                };
-                fetch(`https://just-cold-diver.glitch.me/movies` + movieID, {
-                    method: "PUT",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(editedMovie)
-                })
-                    .then(() => fetch(`https://just-cold-diver.glitch.me/movies`)).then(resp => resp.json()).then(() => getList());
-            }
+//             function editMovie(movieID, title, rating) {
+//                 let editedMovie = {
+//                     title: title,
+//                     rating: rating,
+//                 };
+//                 fetch(`https://just-cold-diver.glitch.me/movies` + movieID, {
+//                     method: "PUT",
+//                     headers: {
+//                         'Content-Type': 'application/json'
+//                     },
+//                     body: JSON.stringify(editedMovie)
+//                 })
+//                     .then(() => fetch(`https://just-cold-diver.glitch.me/movies`)).then(resp => resp.json()).then(() => getList());
+//             }
 
             // submitButton.addEventListener('click', addMovie);
 
 // Edit Rating Function
-            function editRating(rating) {
-                let html = ""
-                if (rating == 1) {
-                    html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
-                    <option selected value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>`
-                    return html;
-                } else if (rating == 2) {
-                    html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
-                    <option value="1">1</option>
-                    <option selected value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>`
-                    return html;
-                } else if (rating == 3) {
-                    html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option selected value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>`
-                    return html;
-                } else if (rating == 4) {
-                    html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option selected value="4">4</option>
-                    <option value="5">5</option>
-                </select>`
-                    return html;
-                } else if (rating == 5) {
-                    html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option selected value="5">5</option>
-                </select>`
-                    return html;
-                }
-                movieList.innerHTML = html;
-            }
+//             function editRating(rating) {
+//                 let html = ""
+//                 if (rating == 1) {
+//                     html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
+//                     <option selected value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option value="3">3</option>
+//                     <option value="4">4</option>
+//                     <option value="5">5</option>
+//                 </select>`
+//                     return html;
+//                 } else if (rating == 2) {
+//                     html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
+//                     <option value="1">1</option>
+//                     <option selected value="2">2</option>
+//                     <option value="3">3</option>
+//                     <option value="4">4</option>
+//                     <option value="5">5</option>
+//                 </select>`
+//                     return html;
+//                 } else if (rating == 3) {
+//                     html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option selected value="3">3</option>
+//                     <option value="4">4</option>
+//                     <option value="5">5</option>
+//                 </select>`
+//                     return html;
+//                 } else if (rating == 4) {
+//                     html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option value="3">3</option>
+//                     <option selected value="4">4</option>
+//                     <option value="5">5</option>
+//                 </select>`
+//                     return html;
+//                 } else if (rating == 5) {
+//                     html += `<select id="selector" class="form-select ratingArea" aria-label="Default select example">
+//                     <option value="1">1</option>
+//                     <option value="2">2</option>
+//                     <option value="3">3</option>
+//                     <option value="4">4</option>
+//                     <option selected value="5">5</option>
+//                 </select>`
+//                     return html;
+//                 }
+//             }
         })
 }
